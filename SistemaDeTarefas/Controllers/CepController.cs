8dc224b0206554c8380e8b861061a1ffdac93ffb
Jair <jair.rollemberg@gmail.com>
2024-04-29 02:27:55 -0300

    Adicionar arquivos de projeto.

---
+++ SistemaDeTarefas/Controllers/CepController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SistemaDeTarefas.Integracao.Interfaces;
+using SistemaDeTarefas.Integracao.Response;
+
+namespace SistemaDeTarefas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CepController : ControllerBase
+    {
+        private readonly IViaCepIntegracao _viaCepIntegracao;
+        public CepController(IViaCepIntegracao viaCepIntegracao)
+        {
+            _viaCepIntegracao = viaCepIntegracao;
+        }
+        [HttpGet]
+        public async Task<ActionResult<ViaCepResponse>> ListarDadosEndereco(string cep)
+        {
+            var responseData = await _viaCepIntegracao.ObterDadosViaCep(cep);
+
+            if (responseData == null)
+            {
+                return BadRequest("CEP não encontrado!");
+            }
+            return Ok(responseData);
+        }
+    }
+}
